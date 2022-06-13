b7bfd56a17bcdb3d65d0f3a003fbf30e6abe2b07
kry-p <jhjung.dev@gmail.com>
2022-06-13 19:31:07 +0900

    Add switch

--- bokeh.js
+++ bokeh.js
@@ -5,32 +5,51 @@
 "use strict";
 
 // bokeh types
-const TYPE_DARK_COLORFUL = 1;
-const TYPE_DARK_GREEN = 2;
-const TYPE_DARK_YELLOW = 3;
-const TYPE_DARK_ORANGE = 4;
-const TYPE_DARK_RED = 5;
-const TYPE_DARK_PURPLE = 6;
-const TYPE_DARK_SKYBLUE = 7;
-const TYPE_BRIGHT_COLORFUL = 8;
-const TYPE_BRIGHT_YELLOW = 9;
-const TYPE_BRIGHT_GREEN = 10;
-const TYPE_BRIGHT_RED = 11;
-const TYPE_BRIGHT_SKYBLUE = 12;
-const TYPE_GRAYSCALE = 13;
+const TYPE_DARK_COLORFUL = 0;
+const TYPE_DARK_GREEN = 1;
+const TYPE_DARK_YELLOW = 2;
+const TYPE_DARK_ORANGE = 3;
+const TYPE_DARK_RED = 4;
+const TYPE_DARK_PURPLE = 5;
+const TYPE_DARK_SKYBLUE = 6;
+const TYPE_BRIGHT_COLORFUL = 7;
+const TYPE_BRIGHT_YELLOW = 8;
+const TYPE_BRIGHT_GREEN = 9;
+const TYPE_BRIGHT_RED = 10;
+const TYPE_BRIGHT_SKYBLUE = 11;
+const TYPE_GRAYSCALE = 12;
+const TYPES = [
+  "Dark multicolored",
+  "Dark green",
+  "Dark yellow",
+  "Dark orange",
+  "Dark red",
+  "Dark purple",
+  "Dark skyblue",
+  "Bright multicolored",
+  "Bright yellow",
+  "Bright green",
+  "Bright red",
+  "Bright skyblue",
+  "Grayscale",
+];
 
 // parallax reaction speed
 // 0(stop) ~ 1(immediate)
 const SPEED = 0.1;
+// Bokeh count
+const DEFAULT_BOKEH_COUNT = 50;
 
 let x = 0;
 let y = 0;
 let mx = 0;
 let my = 0;
 let currentIndex = 0;
+let currentType = 0;
 const bokehs = [];
 const colors = {
   colorful: ["springgreen", "yellow", "orange", "red", "purple", "deepskyblue"],
+  brightColorful: ["rgb(255, 214, 214)", "rgb(230, 230, 214)", "rgb(214, 255, 214)", "rgb(214, 230, 230)", "rgb(214, 214, 255)"],
   bright: ["white"],
 };
 
@@ -47,8 +66,9 @@ const createBokeh = (type) => {
   const generateColor = (type) => {
     switch (type) {
       case TYPE_DARK_COLORFUL:
-      case TYPE_BRIGHT_COLORFUL:
         return colors.colorful[Math.round(Math.random() * 6 - 0.5)];
+      case TYPE_BRIGHT_COLORFUL:
+        return colors.brightColorful[Math.round(Math.random() * 5 - 0.5)];
       case TYPE_DARK_GREEN:
         return colors.colorful[0];
       case TYPE_DARK_YELLOW:
@@ -70,11 +90,12 @@ const createBokeh = (type) => {
   const size = Math.floor(Math.random() * 10 + 2);
   const zIndex = Math.floor(size * 10 - 15);
   const color = generateColor(type);
+  const opacity = type == 7 ? Math.random() * 0.7 + 0.15 : Math.random() * 0.4 + 0.3;
 
   bokeh.id = `bokeh-${currentIndex}`;
   bokeh.style.background = color;
-  bokeh.style.filter = `blur(${Math.floor((1 / zIndex) * 160)}px)`;
-  bokeh.style.opacity = Math.random() * 0.4 + 0.5;
+  bokeh.style.filter = type == 7 ? `saturate(1.25) blur(${Math.floor((1 / zIndex) * 160)}px)` : `blur(${Math.floor((1 / zIndex) * 160)}px)`;
+  bokeh.style.opacity = opacity;
   bokeh.style.position = "absolute";
   bokeh.style.width = `${size}rem`;
   bokeh.style.height = `${size}rem`;
@@ -102,23 +123,78 @@ const pushRandomBokeh = (type) => {
   createBokeh(type);
 };
 
+const setBodyColor = (type) => {
+  switch (type) {
+    case TYPE_BRIGHT_COLORFUL:
+      document.body.style.background =
+        "linear-gradient(180deg, rgba(66,113,145,1) 0%, rgba(116,177,194,1) 35%, rgba(142,155,210,1) 73%, rgba(43,66,118,1) 100%)";
+      break;
+    case TYPE_BRIGHT_YELLOW:
+      document.body.style.background =
+        "linear-gradient(180deg, rgba(240,195,52,1) 0%, rgba(221,168,5,1) 30%, rgba(251,200,54,1) 50%, rgba(251,189,34,1) 100%)";
+      break;
+    case TYPE_BRIGHT_GREEN:
+      document.body.style.background =
+        "linear-gradient(180deg, rgba(142,198,57,1) 0%, rgba(109,174,24,1) 30%, rgba(128,189,59,1) 50%, rgba(100,165,40,1) 100%)";
+      break;
+    case TYPE_BRIGHT_RED:
+      document.body.style.background =
+        "linear-gradient(180deg, rgba(253,85,90,1) 0%, rgba(245,66,73,1) 30%, rgba(239,73,82,1) 50%, rgba(223,57,66,1) 100%)";
+      break;
+    case TYPE_BRIGHT_SKYBLUE:
+      document.body.style.background =
+        "linear-gradient(180deg, rgba(88,187,232,1) 0%, rgba(54,157,207,1) 30%, rgba(73,164,214,1) 50%, rgba(46,130,186,1) 100%)";
+      break;
+    case TYPE_GRAYSCALE:
+      document.body.style.background =
+        "linear-gradient(180deg, rgba(156,155,151,1) 0%, rgba(129,128,122,1) 30%, rgba(132,131,125,1) 50%, rgba(96,95,89,1) 100%)";
+      break;
+    default:
+      document.body.style.background = "rgb(0, 0, 0)";
+  }
+};
+
 const loop = () => {
   mx += (x - mx) * SPEED;
   my += (y - my) * SPEED;
   window.requestAnimationFrame(loop);
 };
 
+const onChangeType = (text, direction) => {
+  if (direction === -1) {
+    currentType === 0 ? (currentType = 12) : (currentType -= 1);
+  } else {
+    currentType = (currentType + 1) % 13;
+  }
+  // remove all bokeh
+  while (bokehs.length > 0) {
+    const bokeh = bokehs.pop();
+    bokeh.bokeh.remove();
+  }
+  text.innerText = TYPES[currentType];
+  for (let i = 0; i < DEFAULT_BOKEH_COUNT; i++) {
+    pushRandomBokeh(currentType);
+  }
+  setBodyColor(currentType);
+};
+
 window.onload = () => {
-  for (let i = 0; i < 50; i++) {
-    pushRandomBokeh(TYPE_DARK_COLORFUL);
+  for (let i = 0; i < DEFAULT_BOKEH_COUNT; i++) {
+    pushRandomBokeh(currentType);
   }
 
   loop();
   const increment = document.getElementById("button-increase-bokeh");
   const decrement = document.getElementById("button-decrease-bokeh");
-  increment.onclick = () => {
-    pushRandomBokeh(TYPE_DARK_COLORFUL);
-  };
+  const left = document.getElementById("button-select-left");
+  const right = document.getElementById("button-select-right");
+  const currentTypeName = document.getElementById("select-item");
+
+  currentTypeName.innerText = TYPES[currentType];
+  increment.onclick = () => pushRandomBokeh(currentType);
+  left.onclick = () => onChangeType(currentTypeName, -1);
+  right.onclick = () => onChangeType(currentTypeName, 1);
+
   decrement.onclick = removeRandomBokeh;
   document.addEventListener("mousemove", reactMouseMovement);
 };
